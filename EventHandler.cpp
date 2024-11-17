f91394201c5397a70d8feb3a5732d6dcc2f8d79a
Hunter Chen <hello@hunterchen.ca>
2024-11-17 05:25:35 -0500

    Revert "switch to qmake"
    This reverts commit 1ed4dc30bf18cb481aae4992301ee4bfd81b75a2.

---
+++ EventHandler.cpp
@@ -0,0 +1,16 @@
+#include "EventHandler.h"
+#include <QDebug>
+
+EventHandler::EventHandler(QObject *parent) : QObject(parent) {}
+
+void EventHandler::handlePreviousImage(QLabel *label) {
+    qDebug() << "Previous image action triggered";
+}
+
+void EventHandler::handleNextImage(QLabel *label) {
+    qDebug() << "Next image action triggered";
+}
+
+void EventHandler::handleRating(int rating, QLabel *label) {
+    qDebug() << "Image rated with:" << rating;
+}
