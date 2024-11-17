1ed4dc30bf18cb481aae4992301ee4bfd81b75a2
Hunter Chen <hello@hunterchen.ca>
2024-11-17 05:19:57 -0500

    switch to qmake

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,52 +1,14 @@
-#include "MainWindow.h"
-#include "./ui_mainwindow.h"
-#include <QPixmap>
-#include <QKeyEvent>
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent, ImageController *controller)
+MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
-    , imageLabel(new QLabel(this))
-    , imageController(controller)  // Initialize controller
 {
     ui->setupUi(this);
-
-    // Display the first image
-    QPixmap pixmap(QString::fromStdString(imageController->getCurrentFile()));
-
-    if (!pixmap.isNull()) {
-        imageLabel->setPixmap(pixmap.scaled(800, 600, Qt::KeepAspectRatio));
-        imageLabel->setAlignment(Qt::AlignCenter);
-        setCentralWidget(imageLabel);
-    } else {
-        imageLabel->setText("Failed to load image");
-        imageLabel->setAlignment(Qt::AlignCenter);
-        setCentralWidget(imageLabel);
-    }
 }
 
-MainWindow::~MainWindow() {
+MainWindow::~MainWindow()
+{
     delete ui;
-}
-
-void MainWindow::keyPressEvent(QKeyEvent *event) {
-    switch (event->key()) {
-    case Qt::Key_Left:
-        imageController->previousImage();
-        break;
-    case Qt::Key_Right:
-        imageController->nextImage();
-        break;
-    default:
-        QMainWindow::keyPressEvent(event);
-        break;
-    }
-
-    // Update the displayed image
-    QPixmap pixmap(QString::fromStdString(imageController->getCurrentFile()));
-    if (!pixmap.isNull()) {
-        imageLabel->setPixmap(pixmap.scaled(800, 600, Qt::KeepAspectRatio));
-    } else {
-        imageLabel->setText("Failed to load image");
-    }
 }
---
+++ viewr.pro
@@ -0,0 +1,33 @@
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+CONFIG += c++17
+
+# You can make your code fail to compile if it uses deprecated APIs.
+# In order to do so, uncomment the following line.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+SOURCES += \
+    imagecontroller.cpp \
+    main.cpp \
+    mainwindow.cpp
+
+HEADERS += \
+    imagecontroller.h \
+    mainwindow.h
+
+FORMS += \
+    mainwindow.ui
+
+# Default rules for deployment.
+qnx: target.path = /tmp/$${TARGET}/bin
+else: unix:!android: target.path = /opt/$${TARGET}/bin
+!isEmpty(target.path): INSTALLS += target
+
+win32:CONFIG(release, debug|release): LIBS += -LD:/Downloads/exiv2-0.28.3-2019msvc64/release/ -lexiv2
+else:win32:CONFIG(debug, debug|release): LIBS += -LD:/Downloads/exiv2-0.28.3-2019msvc64/debug/ -lexiv2
+else:unix: LIBS += -LD:/Downloads/exiv2-0.28.3-2019msvc64/ -lexiv2
+
+INCLUDEPATH += D:/Downloads/exiv2-0.28.3-2019msvc64/include
+DEPENDPATH += D:/Downloads/exiv2-0.28.3-2019msvc64/include
